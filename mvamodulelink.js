be64498c26ed60c10253b3d42f7578d4f10a636f
Eric ShangKuan <ericsk@gmail.com>
2014-10-12 17:53:28 +0800

    Adapte with MVA Oct,2014 update

--- mvamodulelink.js
+++ mvamodulelink.js
@@ -1,14 +1,12 @@
 (function(){
-  // 
-  var reg = /expandModuleContainer\((\d+)?, true\)/;
   // MVA course URL
   var link_tmpl = ["http://", location.hostname, location.pathname].join('');
     
-  $('.module').each(function(){
-    var matches = $(this).attr('onclick').match(reg);
-    var courseId = matches[1];
+  $('.module-container').each(function(){
+    var courseId = $(this).attr('id');
     var link = [link_tmpl, "?m=", courseId].join('');
     
+    // add a helper link in module content
     $('#container-' + courseId + ' > ul.list-ctypes').prepend('<li><div class="content-item"><a href="'+link+'" target="_blank" class="mva-module-link">[link to this module]</a></div></li>');
     
   });
